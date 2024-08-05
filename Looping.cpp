9d04dda02a9e3f6a352db455782667abca772a87
Jayesh Patil <jayeshkishorpatil12@gmail.com>
2024-08-05 22:46:20 +0530

    Looping.cpp

--- Looping.cpp
+++ Looping.cpp
@@ -1,7 +1,10 @@
+// BASIC SYNTAX FOR CREATING A LOOP IN C++
+// THE CONDITIONS FOR FOR LOOP ARE USER DEFINED
+
 #include <iostream> 
 using namespace std;
 int main (){
     for (int x = 0; x<5 ; x++){
-        cout<<"ASHISH\n";
+        cout<<"LOOP\n";
     }
 }
